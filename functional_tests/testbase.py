814f2548ae060a03fa6b13e4a13709513350ae50
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-19 18:53:11 +0000

    just an initial unit test for checking the infrastructure

---
+++ functional_tests/testbase.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : cloud-web-frontend
+# Timestamp : 19-03-2018 18:52
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
