25313c29eff6e0b43e80850124a885f50a5c21bf
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-19 19:27:25 +0000

    application wide exceptions, placeholder

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+# 
+# Project   : cloud-web-frontend
+# Timestamp : 19-03-2018 19:27
+# Author    : Manuel Bernal Llinares
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
