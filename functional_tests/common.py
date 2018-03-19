a16e764a4fb6c84b2217647dfee05aa11e521777
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-19 19:34:30 +0000

    get browser instance, get a logger

--- functional_tests/common.py
+++ functional_tests/common.py
@@ -11,9 +11,19 @@
 Common environment file for running functional tests
 """
 
+import os
+import uuid
+from selenium import webdriver
+# Application imports
+import config_manager
+import toolbox.general as general_toolbox
+
+
+# Some globals
 __browser_profile_counter = 0
 
 
 def get_browser_instance():
-    pass
+    logger = config_manager.get_app_config_manager().get_logger_for("{}.{}".format(__name__, "get_browser_instance"))
+
 
