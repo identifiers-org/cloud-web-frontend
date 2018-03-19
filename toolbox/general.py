986be16258333a90931964df4f121aae9a8efb9f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-19 19:30:57 +0000

    general toolbox, read json

--- toolbox/general.py
+++ toolbox/general.py
@@ -18,3 +18,16 @@ import subprocess
 # App modules
 from exceptions import ToolBoxException
 
+
+
+def read_json(json_file="json_file_not_specified.json"):
+    """
+    Reads a json file and it returns its object representation, no extra checks
+    are performed on the file so, in case anything happens, the exception will
+    reach the caller
+    :param json_file: path to the file in json format to read
+    :return: an object representation of the data in the json file
+    """
+    with open(json_file) as jf:
+        return json.load(jf)
+
