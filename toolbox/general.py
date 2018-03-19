cdc88eef86cedd6c33381f604f1cb89201099da3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-19 19:31:31 +0000

    general toolbox, check-create folders overwrite

--- toolbox/general.py
+++ toolbox/general.py
@@ -47,3 +47,30 @@ def check_create_folders(folders):
         else:
             if not os.path.isdir(folder):
                 raise ToolBoxException("'{}' is not a folder".format(folder))
+
+
+def check_create_folders_overwrite(folders):
+    """
+    Given a list of folders, this method will create them, overwriting them in case they exist
+    :param folders: list of folders to create
+    :return: no return value
+    :except: if any element in the list of folders is not a folder, an exception will be raised
+    """
+    invalid_folders = []
+    for folder in folders:
+        if os.path.exists(folder):
+            if not os.path.isdir(folder):
+                invalid_folders.append(folder)
+    if invalid_folders:
+        # If there's any invalid folder, we don't make any change, and we report the situation by raising an exception
+        raise ToolBoxException("The following folders ARE NOT FOLDERS - '{}'"
+                               .format(invalid_folders))
+    for folder in folders:
+        try:
+            shutil.rmtree(folder)
+        except FileNotFoundError as e:
+            # It is find if the folder is not there
+            pass
+    check_create_folders(folders)
+
+
