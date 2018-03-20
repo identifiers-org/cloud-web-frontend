fb021607d71d0333e51d9a2630a2d8f7fdaf5147
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-20 15:09:19 +0000

    get the resolver location from the environment

--- app_search/models.py
+++ app_search/models.py
@@ -13,6 +13,7 @@ class HomePage:
 class ResolutionServiceLocationFactory:
     @staticmethod
     def get_resolver_host_and_port():
-        pass
+        return (os.environ.get('WEB_CONFIG_SERVICE_RESOLVER_HOST', 'localhost'),
+                os.environ.get('WEB_CONFIG_SERVICE_RESOLVER_PORT', '8080'))
 
 
