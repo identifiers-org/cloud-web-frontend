f0b4d7df29da3e4f10ffc701f54096fac37cf42d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-20 04:19:11 +0000

    test_this_app_is_the_root_landing_page() test case

--- app_search/tests.py
+++ app_search/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from app_search.views import home_page
+
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
-    pass
+
+    def test_this_app_is_the_root_landing_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
