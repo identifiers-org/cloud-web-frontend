fe7c2f29b54d52ad8f5e81a58c8596033e7d2fdc
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-20 14:40:23 +0000

    wiring javascript into the webpage

--- app_search/static/js/app_search_home.js
+++ app_search/static/js/app_search_home.js
@@ -11,22 +11,44 @@ var AppSearchHomePageGlue = (function () {
     function initPage() {
         // This function hooks all the events to the webpage
         console.debug("Initialize page Javascript...")
+        var resolutionRequestController = new ResolutionRequestController("resolution_form",
+            "resolve_input_box",
+            "btn_resolve",
+            "btn_resolve_lucky")
     }
 
     // Resolution request controller
-    function ResolutionRequestController(inputTextId, btnResolveId, btnLuckyId) {
-        // TODO
+    function ResolutionRequestController(resolutionFormId, inputTextId, btnResolveId, btnLuckyId) {
+        this.resolutionFormId = resolutionFormId;
         this.inputTextId = inputTextId;
         this.btnResolveId = btnResolveId;
         this.btnLuckyId = btnLuckyId;
+        console.debug("Wiring the Resolution Request Controller");
+        // Attach events
+        document.getElementById(resolutionFormId).addEventListener("submit", this.formSubmit);
+        document.getElementById(btnResolveId).addEventListener("click", this.clickBtnResolve);
+        document.getElementById(btnLuckyId).addEventListener("click", this.clickBtnLucky);
     }
 
-    ResolutionRequestController.prototype.clickBtnLucky = function () {
+    ResolutionRequestController.prototype.clickBtnLucky = function (event) {
+        event.preventDefault();
         // TODO
+        console.debug("I'm gonna be lucky CLICKED");
+        return false;
     };
 
-    ResolutionRequestController.prototype.clickBtnResolve = function () {
+    ResolutionRequestController.prototype.clickBtnResolve = function (event) {
+        event.preventDefault();
         // TODO
+        console.debug("Resolve Button CLICKED");
+        return false;
+    };
+
+    ResolutionRequestController.prototype.formSubmit = function (event) {
+        // TODO
+        event.preventDefault();
+        console.debug("Hit enter in resolution request form");
+        return false;
     };
     // END --- Resolution request controller
 
