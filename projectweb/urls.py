b2ca1ad5961fb65689060739492642e0dbb7cfdc
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-20 04:23:52 +0000

    bug fix

--- projectweb/urls.py
+++ projectweb/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app_search import app_search_views
+from app_search import views as app_search_views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
