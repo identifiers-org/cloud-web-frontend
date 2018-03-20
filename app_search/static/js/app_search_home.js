cf37adbdf356ee88e018cfdac4f613a9fbb84e8e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-20 12:19:35 +0000

    javascript glue for the resolution home page

---
+++ app_search/static/js/app_search_home.js
@@ -0,0 +1,8 @@
+/**
+ * Project: cloud-web-frontend
+ * Timestamp: 2018-03-20 12:19
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * Javascript glue for the resolution page.
+ */
