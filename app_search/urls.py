4deef2311c9b38a2858f771673dcf848a39e16b0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-20 11:43:21 +0000

    application specific url routing, placeholder

---
+++ app_search/urls.py
@@ -0,0 +1,12 @@
+# 
+# Project   : cloud-web-frontend
+# Timestamp : 20-03-2018 11:43
+# Author    : Manuel Bernal Llinares
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
