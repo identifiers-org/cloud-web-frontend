09e6035867458c7e920a934249d86f311ec982f8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-20 05:45:32 +0000

    test refactoring

--- app_search/tests.py
+++ app_search/tests.py
@@ -7,6 +7,6 @@ from app_search.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_this_app_is_the_root_landing_page(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
