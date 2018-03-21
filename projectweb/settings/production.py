4205b183fbb31888b0a673d2012a077eb17280e3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-21 13:17:49 +0000

    production environment settings for django

---
+++ projectweb/settings/production.py
@@ -0,0 +1,12 @@
+# 
+# Project   : cloud-web-frontend
+# Timestamp : 21-03-2018 13:17
+# Author    : Manuel Bernal Llinares
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
