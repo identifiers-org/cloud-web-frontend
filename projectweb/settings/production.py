fd33bc5a561ace5694e217df34e4b86070e63bd4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-21 13:35:22 +0000

    comments

--- projectweb/settings/production.py
+++ projectweb/settings/production.py
@@ -15,10 +15,11 @@ import os
 from .base import *
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').strip().split(',')
-SECURE_HSTS_SECONDS = 0
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 # We don't do redirect because this is designed to be deployed behing an SSL termination load balancer
 SECURE_SSL_REDIRECT = False
 # Thus, there is no SSL Host
 SECURE_SSL_HOST = None
+# And nothing to do with any of the other SSL based settings
+SECURE_HSTS_SECONDS = 0
