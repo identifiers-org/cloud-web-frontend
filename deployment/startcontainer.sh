7057ea77e75366dc248d7c4f1321a51caf63142b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-22 03:54:33 +0000

    configure ngingx for the running site

--- deployment/startcontainer.sh
+++ deployment/startcontainer.sh
@@ -2,3 +2,5 @@
 # Container start up script
 # Author: Manuel Bernal Llinares <mbdebian@gmail.com>
 
+echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX site, ${SITE_CNAME}"
+sed -i -- "s/SITE_CNAME/${SITE_CNAME}/g" deployment/nginx.conf
