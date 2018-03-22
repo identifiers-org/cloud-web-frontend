14c43cff3c9ab83bcf4f4fb826bce48115807268
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-22 07:17:03 +0000

    isolate site specific configuration in a file

--- deployment/startcontainer.sh
+++ deployment/startcontainer.sh
@@ -12,8 +12,9 @@ echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX proxy pass using unix sockets,
 sed -i -- "s~PROXY_PASS_INFORMATION~${PROXY_PASS_INFORMATION}~g" deployment/nginx.site
 
 echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX logging subsystem"
-sed -i '/access_log/c\\taccess_log \/dev\/stdout' /etc/nginx/nginx.conf
-sed -i '/error_log/c\\terror_log \/dev\/stderr' /etc/nginx/nginx.conf
+#sed -i '/access_log/c\\taccess_log \/dev\/stdout' /etc/nginx/nginx.conf
+#sed -i '/error_log/c\\terror_log \/dev\/stderr' /etc/nginx/nginx.conf
+ln -s ${FOLDER_WEBSITE_ROOT}/deployment/nginx.conf /etc/nginx/conf.d/site.conf
 
 echo "<===|DEVOPS|===> [CRYPTO] Generating a random secret key for Django"
 export DJANGO_SECRET_KEY=$(python3 -c "import string,random; uni=string.ascii_letters+string.digits+string.punctuation; print(repr(''.join([random.SystemRandom().choice(uni) for i in range(random.randint(45,50))])))")
