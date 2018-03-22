0c9ca2e2667a8209cb93fd8a490f977969acc4d8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-22 06:41:05 +0000

    nginx site conf file renamed, refactoring

--- deployment/startcontainer.sh
+++ deployment/startcontainer.sh
@@ -3,13 +3,13 @@
 # Author: Manuel Bernal Llinares <mbdebian@gmail.com>
 
 echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX site, ${SITE_CNAME}"
-sed -i -- "s/SITE_CNAME/${SITE_CNAME}/g" deployment/nginx.conf
+sed -i -- "s/SITE_CNAME/${SITE_CNAME}/g" deployment/nginx.site
 
 echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX static content location, ${DJANGO_STATIC_ROOT}"
-sed -i -- "s~DJANGO_STATIC_ROOT~${DJANGO_STATIC_ROOT}~g" deployment/nginx.conf
+sed -i -- "s~DJANGO_STATIC_ROOT~${DJANGO_STATIC_ROOT}~g" deployment/nginx.site
 
 echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX proxy pass using unix sockets, ${PROXY_PASS_INFORMATION}"
-sed -i -- "s~PROXY_PASS_INFORMATION~${PROXY_PASS_INFORMATION}~g" deployment/nginx.conf
+sed -i -- "s~PROXY_PASS_INFORMATION~${PROXY_PASS_INFORMATION}~g" deployment/nginx.site
 
 echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX logging subsystem"
 sed -i '/access_log/c\\taccess_log \/dev\/stdout' /etc/nginx/nginx.conf
