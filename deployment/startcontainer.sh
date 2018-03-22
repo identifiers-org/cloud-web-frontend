da6bae846c8c4c20939aa48d624311241540597b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-22 06:27:00 +0000

    bug fix

--- deployment/startcontainer.sh
+++ deployment/startcontainer.sh
@@ -6,7 +6,7 @@ echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX site, ${SITE_CNAME}"
 sed -i -- "s/SITE_CNAME/${SITE_CNAME}/g" deployment/nginx.conf
 
 echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX static content location, ${DJANGO_STATIC_ROOT}"
-sed -i -- "s~DJANGO_STATIC_ROOT~${DJANGO_STATIC_ROOT}g" deployment/nginx.conf
+sed -i -- "s~DJANGO_STATIC_ROOT~${DJANGO_STATIC_ROOT}~g" deployment/nginx.conf
 
 echo "<===|DEVOPS|===> [CONFIG] Configuring NGINX proxy pass using unix sockets, ${PROXY_PASS_INFORMATION}"
 sed -i -- "s~PROXY_PASS_INFORMATION~${PROXY_PASS_INFORMATION}~g" deployment/nginx.conf
