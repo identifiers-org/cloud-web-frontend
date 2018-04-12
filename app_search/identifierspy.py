96077ff11ce04cd5f6ec4b58ce54dd2e121e59e3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-04-12 11:57:31 +0100

    RESOLVED_RESOURCE_JSON_KEY_INSTITUTION constant

--- app_search/identifierspy.py
+++ app_search/identifierspy.py
@@ -72,6 +72,7 @@ class ResolvedResource:
     RESOLVED_RESOURCE_JSON_KEY_RESOURCE_PREFIX = 'resourcePrefix'
     RESOLVED_RESOURCE_JSON_KEY_ACCESS_URL = 'accessUrl'
     RESOLVED_RESOURCE_JSON_KEY_INFO = 'info'
+    RESOLVED_RESOURCE_JSON_KEY_INSTITUTION = 'institution'
 
     def __init__(self, json_data=None):
         self.id = ""
