3940fec4b5e6f25b56c6e0dbad06c0096e7249dd
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-04-12 11:55:10 +0100

    refactor

--- app_search/identifierspy.py
+++ app_search/identifierspy.py
@@ -33,7 +33,7 @@ class ServerRequest:
 
 # Server Response model
 class ServerResponse:
-    SERVER_RESPONSE_KEY_API_VERSION = 'apiVersion'
+    SERVER_RESPONSE_JSON_KEY_API_VERSION = 'apiVersion'
     SERVER_RESPONSE_KEY_ERROR_MESSAGE = 'errorMessage'
     SERVER_RESPONSE_KEY_PAYLOAD = 'payload'
 
@@ -43,8 +43,8 @@ class ServerResponse:
         self.http_status = 0
         self.payload = {}
         if json_data:
-            if self.SERVER_RESPONSE_KEY_API_VERSION in json_data:
-                self.api_version = json_data[self.SERVER_RESPONSE_KEY_API_VERSION]
+            if self.SERVER_RESPONSE_JSON_KEY_API_VERSION in json_data:
+                self.api_version = json_data[self.SERVER_RESPONSE_JSON_KEY_API_VERSION]
                 # TODO - We should check the API Version in the response matches the one supported here
             if self.SERVER_RESPONSE_KEY_ERROR_MESSAGE in json_data:
                 self.error_message = json_data[self.SERVER_RESPONSE_KEY_ERROR_MESSAGE]
