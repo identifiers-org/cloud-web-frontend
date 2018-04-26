f418553359bf0250d6640dd2c8d823dfe48621ce
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-04-26 11:14:24 +0100

    allow all CORS

--- projectweb/settings/production.py
+++ projectweb/settings/production.py
@@ -16,6 +16,7 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').strip().split(',')
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').strip().split(',')
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
