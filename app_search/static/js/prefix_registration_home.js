8b9a50bcc3c1ce7b949264e563b738bf56fccbda
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-04-28 08:52:45 +0100

    validateFormField() placeholder

--- app_search/static/js/prefix_registration_home.js
+++ app_search/static/js/prefix_registration_home.js
@@ -25,6 +25,12 @@ var PrefixRegistrationWebPageGlue = (function () {
     var prefixRegistrationFormItemRequesterEmail = "prefixRegistrationRequesterEmail";
     // Registry Service
     var registryService;
+
+    // Validation function
+    function validateFormField(event) {
+        // TODO
+    }
+
     function initPage() {
         // TODO - Glue all the web page elements
         registryService = IdentifiersJS.getRegistry(prefixRegistrationWebServiceHost, prefixRegistrationWebServicePort);
